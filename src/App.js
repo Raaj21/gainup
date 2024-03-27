7d5499002ddb2c9b2efefc584681a51cc7484e12
raaj21 <raaj92.m@gmail.com>
2024-03-27 18:07:02 +0530

    banner design correction 2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 // App.js
 
-import React from 'react';
-import './App.css';
-import Header from './Header';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomeScreen from './Homescreen';
-import CateringComponent from './CateringComponent';
-import VideoComponent from './Video';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import CateringComponent from "./CateringComponent";
+import VideoComponent from "./Video";
 
 function App() {
   return (
     <Router>
       <Header /> {/* Always display the header */}
       <Routes>
-        <Route path="/" element={ <CateringComponent/>} />
+        <Route path="/" element={<CateringComponent />} />
         <Route path="/Video" element={<VideoComponent />} />
         {/* Add more routes as needed */}
       </Routes>
